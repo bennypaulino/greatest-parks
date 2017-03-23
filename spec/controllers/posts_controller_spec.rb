2dfbf148ad010d503debf665b426a21d3ae1e8dc
bennypaulino <santiagopro71@gmail.com>
2017-03-23 17:55:39 -0400

    Allow Posts to be created

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "posts#create action" do
+    it "should successfully create a new post in the db" do
+      post :create, post: {title: 'Hello', body: 'New post!'}
+      expect(response).to redirect_to root_path
+
+      post = Post.last
+      expect(post.body).to eq('New post!')
+      expect(post.title).to eq('Hello')
+    end
+  end
 end
